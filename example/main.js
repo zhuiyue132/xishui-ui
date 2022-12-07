629b11863088ad1f51028e2d4fe2043a81675ec1
zhuiyue132 <fenda.app@qq.com>
2022-12-07 18:19:21 +0800

    fix: 修改目录以适配按需加载;

--- example/main.js
+++ example/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import '@xishui-ui/theme-chalk/src/index.scss';
-import XSUI from '@xishui-ui/components';
+import '../packages/theme-chalk/src/index.scss';
+import XSUI from '../packages/xishui-ui/index.js';
 const app = createApp(App);
 app.use(XSUI);
 app.mount('#app');
--- packages/components/index.js
+++ packages/components/index.js
@@ -1,13 +1,2 @@
-import * as components from './components';
-import { version } from './package.json';
-
-const install = function (app) {
-  Object.entries(components).forEach(([key, value]) => {
-    app.component(key, value);
-  });
-};
-
-export default {
-  install,
-  version
-};
+export * from './button';
+export * from './config-provider';
